3808f8e1c6ebc14d33ceac5e58c54d8cd5db8441
Jorma <joel.oksanen1223@gmail.com>
2023-03-20 17:29:51 +0200

    Erilliset functions- ja header-tiedostot

--- index.php
+++ index.php
@@ -1,22 +1,11 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Credentials: true: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Max-Age: 3600');
-
-header('Content-Type: application/json');
-
-if ($_SERVER['REQUEST_METHOD']=== 'OPTIONS') {
-    return 0;
-}
+require_once 'inc/functions.php';
+require_once 'inc/header.php';
 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8', 'root', '');
-
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
+ 
+    $db = openDb();
     $sql = "select * from item";
     $query = $db->query($sql);
     
@@ -24,11 +13,8 @@ try {
     header('HTTP/1.1 200 OK');
     
     print json_encode($results);
+
 } catch (PDOException $pdoex) {
 
-    header('HTTP/1.1 500 Internal Server Error');
-    $error = array('error' => $pdoex->getMessage());
-    print json_encode($error);
+    returnError($pdoex);
 }
-
-
